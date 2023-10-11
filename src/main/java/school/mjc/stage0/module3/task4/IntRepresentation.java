25cd64531f08be36deaf3d6cea2e4681a2321ad3
Alexeykaa <ka.alexey@gmail.com>
2023-10-11 19:54:33 +0300

    fix test

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] x = {99, 104, 97, 114};
+        for (int i : x) {
+            System.out.println((char) i);
+        }
     }
 }
